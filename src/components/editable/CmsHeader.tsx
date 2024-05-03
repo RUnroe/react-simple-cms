f6c375aeb634388120f8bd372ec8b030a408e69f
Ryan Unroe <ryanunroe@gmail.com>
2024-05-03 09:56:39 -0600

    Update props of editable components

--- src/components/editable/CmsHeader.tsx
+++ src/components/editable/CmsHeader.tsx
@@ -6,25 +6,29 @@ export interface CmsHeaderProps extends EditableComponent {
   inputs: {
     type: HeaderType,
     text: string,
-    classNames?: string[],
+    style: object,
   }
 }
 
 
 export const CmsHeader = ({
-  //EditableComponent
-  className, containerClassName, inEditMode = false,
   //This components input fields
-  inputs
+  inputs,
+  //EditableComponent
+  className, 
+  ...rest
   
-}: CmsHeaderProps,) => {
+}: CmsHeaderProps) => {
 
 
   return (
-    <div className={`cms-component header-cms-component ${containerClassName} ${inEditMode ? "edit-mode" : ""}`}>
-      <ComponentWrapper tag={inputs.type as unknown as keyof JSX.IntrinsicElements} className={`${className} ${inputs?.classNames?.join(" ") || ""}`}>
-        {inputs.text}
-      </ComponentWrapper>
-    </div>
+    <ComponentWrapper 
+      tag={inputs.type as unknown as keyof JSX.IntrinsicElements} 
+      className={`cms-component header-cms-component ${className}`}
+      style={inputs.style}
+      {...rest}
+    >
+      {inputs.text}
+    </ComponentWrapper>
   )
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,3 @@
 export interface EditableComponent {
   className?: string,
-  containerClassName?: string,
-  inEditMode: boolean,
 }
